d9d42e0811b0a665aa7316cd7524d616e9867051
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-07-01 15:17:27 -0400

    deleted goblin.py and hero.py

--- Hero.py
+++
@@ -1,8 +0,0 @@
-class Hero():
-    def __init__(self, health, power):
-        self.health = health
-        self.power = power
-    
-    def attack(self, enemy):
-        self.health -= enemy.power
-        enemy.health -= self.power
